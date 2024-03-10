2cd5ef2878014108968ba551971f7c02c60723d8
ShobhitPatra <shobhit9999999@gmail.com>
2024-03-10 23:41:00 +0530

    filtered users

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Button from "./Button";
+import axios from "axios";
+
+export default function Users() {
+  const [filter, setFilter] = useState("");
+  const [users, setusers] = useState([]);
+
+  useEffect(() => {
+    const response = axios.get(
+      "http://localhost:3000/api/v1/user/bulk?filter=" + filter
+    );
+    const desiredUsers = response.data;
+    setusers(desiredUsers);
+  }, [filter]);
+  return (
+    <>
+      <div id="users-outer-container" className="flex flex-col w-screen">
+        <div id="users-title" className="xl:text-2xl font-semibold m-2">
+          Users
+        </div>
+        <div id="input" className="flex justify-between bg-red-600">
+          <input
+            type="text"
+            placeholder="search users"
+            className="w-full p-2 text-xl m-2 rounded-lg "
+            onChange={(e) => {
+              setFilter(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div id="filtered-users"></div>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Appbar from "../components/Appbar";
 import Balance from "../components/Balance";
+import Users from "../components/Users";
 
 export default function Dashboard() {
   return (
     <>
-      <div>
+      {/* <div>
         <Appbar />
       </div>
       <div>
         <Balance />
+      </div> */}
+      <div>
+        <Users />
       </div>
     </>
   );
