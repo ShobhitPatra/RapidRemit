b5c2d542af05e9bf3068a768a65cead4ccf28079
ShobhitPatra <shobhit9999999@gmail.com>
2024-04-19 22:41:42 +0530

    transfer amount controller

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import { Account } from "../models/account.model.js";
+import { User } from "../models/user.model.js";
 
 export const balance = async (req, res) => {
   try {
@@ -22,4 +24,51 @@ export const balance = async (req, res) => {
   }
 };
 
-export const transfer = (req, res) => {};
+export const transfer = async (req, res) => {
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const { amount, toUserId } = req.body;
+
+    const fromUserId = req.user._id;
+    const fromAccount = await Account.findOne({
+      accountHolder: fromUserId,
+    }).session(session);
+
+    if (!fromAccount || fromAccount.balance < amount) {
+      await session.abortTransaction();
+      res.status(400).json({
+        msg: "insufficient balance",
+      });
+    }
+
+    const toAccount = await Account.findOne({ accountHolder: to }).session(
+      session
+    );
+
+    if (!fromAccount) {
+      await session.abortTransaction();
+      res.status(400).json({
+        msg: "recievers account not found",
+      });
+    }
+
+    await Account.updateOne(
+      { accountHolder: fromUserId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+
+    await Account.updateOne(
+      {
+        accountHolder: toUserId,
+      },
+      { $inc: { balance: amount } }
+    ).session(session);
+    await session.commitTransaction();
+    res.status(200).json({
+      message: "Transfer successful",
+    });
+  } catch (error) {
+    console.log("error in transfer controller : ", error.message);
+  }
+};
