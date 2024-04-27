5cae926c3dc2d574e2ced78e5f71dded844538d6
ShobhitPatra <shobhit9999999@gmail.com>
2024-04-27 23:35:21 +0530

    home ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div className="h-screen bg-teal-900">sbkjfgkjhb</div>;
+  return (
+    <>
+      <div className="flex flex-col  min-w-96 mx-auto items-center justify-center bg-slate-600"></div>
+    </>
+  );
 };
 
 export default Home;
